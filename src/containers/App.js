c39b7ed79e09c2438d78948de12a32a1905443ee
mcooperstein <mcooperstein@gmail.com>
2024-11-21 22:00:19 -0800

    adding redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,39 +4,83 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css'; 
 
-
-function App () {
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState('');
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response=> response.json())
-    .then(users => {setRobots(users)});
-    console.log(count);
-  }, [count])
-
-  const onSearchChange = (event) => {
-     setSearchfield(event.target.value)
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchField: '',
+      count: 0
+    }
   }
 
-  const filteredRobots = robots.filter(robot =>{
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-  })
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+      console.log(response);
+      return response.json();
+    }).then(users => {
+      console.log(users)
+      this.setState({robots: users})
+    })
+  }
 
-  return !robots.length ?
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value})
+  }
+
+  render() {
+    let filteredRobots = this.state.robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
+    return !this.state.robots.length ?
     <h1>Loading</h1> :
     (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
         <button onClick={() => setCount(count + 1)}>Click Me!</button>
-        <SearchBox searchChange={onSearchChange}/>
+        <SearchBox searchChange={this.onSearchChange}/>
         <Scroll>
           <CardList robots={filteredRobots} />
         </Scroll>
       </div>
     );
+  }
+
 }
+
+
+// function App () {
+//   const [robots, setRobots] = useState([]);
+//   const [searchfield, setSearchfield] = useState('');
+//   const [count, setCount] = useState(0)
+
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response=> response.json())
+//     .then(users => {setRobots(users)});
+//     console.log(count);
+//   }, [count])
+
+//   const onSearchChange = (event) => {
+//      setSearchfield(event.target.value)
+//   }
+
+//   const filteredRobots = robots.filter(robot =>{
+//     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//   })
+
+//   return !robots.length ?
+//     <h1>Loading</h1> :
+//     (
+//       <div className='tc'>
+//         <h1 className='f1'>RoboFriends</h1>
+//         <button onClick={() => setCount(count + 1)}>Click Me!</button>
+//         <SearchBox searchChange={onSearchChange}/>
+//         <Scroll>
+//           <CardList robots={filteredRobots} />
+//         </Scroll>
+//       </div>
+//     );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
+// import { createRoot } from "react-dom/client"; replaced by ReactDOM.render
+import { Provider, connect } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import './index.css'; 
 import App from './containers/App';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
+
+const store = configureStore({
+    reducer: {
+        search: searchRobots
+    }
+});
+
+// const root = createRoot(document.getElementById('root'));
+// root.render(<App />);
 
 
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+registerServiceWorker();
